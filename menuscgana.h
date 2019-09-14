5e66796fccd4acfa7144d8e251e20c70915c5eb7
danrleydaniel <53260793+danrleydaniel@users.noreply.github.com>
2019-09-14 12:20:24 -0300

    Create menuscgana.h
    Extensão .h da biblioteca de menus.

---
+++ menuscgana.h
@@ -0,0 +1,5 @@
+void logotipoPrincipal(void); //Mostra o logotipo do programa principal
+char mainMenu(void); //Menu principal do programa
+char menuClientes(void); //Mostra o menu da área dos clientes
+char menuPrevisoes(void); //Mostra o menu da área de previsões
+void creditos(void); //Informações sobre os desenvolvedores e o programa
