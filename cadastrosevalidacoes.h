11ace04f4fce9ebeb980a1ca3bcf250c8564d513
danrleydaniel <53260793+danrleydaniel@users.noreply.github.com>
2019-09-14 12:14:23 -0300

    Create cadastrosevalidacoes.h
    Extensão .h da biblioteca responsável por cadastrar e validar informações dados pelo usuário.

---
+++ cadastrosevalidacoes.h
@@ -0,0 +1,8 @@
+void cdCliente(void); //Função que cadastra um cliente
+int numerologia(int, int, int); //Função que retorna o "número nativo" do usário
+char * horoscopo(int); //Função que retorna o animal no horóscopo chinês correspondente à data de nascimento do usuário
+char * signo(int, int, int); //Função que retorna o signo de acordo com a data de nascimento do usuário
+int bissexto(int); //Função do professor Flavius Gorgônio que checa se um ano é bissexto
+int dataValida(int, int, int); //Função do professor Flavius Gorgônio que checa se uma data fornecida pelo usário é válida
+int validaCpf(char *); //Função que verifica a validez de um CPF dado pelo usário
+int validaEmail(char *); //Função que verifica a validez de um email dado pelo usário
