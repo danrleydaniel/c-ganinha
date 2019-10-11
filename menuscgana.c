b579f472e65838bb7053301f98dbd60447a13545
danrleydaniel <danrleydaniel21@gmail.com>
2019-10-11 11:34:15 -0300

    Update menuscgana.c
    Criação de novas "logos" para o Menu Clientes e a opção Cadastrar Cliente;
    Melhorias no cadastro de clientes (utilizando estruturas);
    Criação de uma função "Listar Clientes" (ainda sob implementação).

--- menuscgana.c
+++ menuscgana.c
@@ -7,12 +7,27 @@
 #include <string.h>
 #include <ctype.h>
 #include "cadastrosevalidacoes.h"
+#include "menuscgana.h"
 
 void logotipoPrincipal(void){
  	printf("//////////////////////////////////\n");
 	printf("//     SANDRA ROSA MADALENA     //\n");
 	printf("//////////////////////////////////");
 	printf("\n\n");
+}
+
+void logoCadastrarCliente(void){
+  printf("==========================\n"
+"||CADASTRAR NOVO USUÁRIO||\n"
+"==========================\n"
+"");
+}
+
+void logoMenuClientes(void){
+  printf("===================\n"
+"|| MENU CLIENTES ||\n"
+"===================\n"
+"");
 }
 
 char mainMenu(void){
@@ -23,29 +38,34 @@ char mainMenu(void){
 	printf("3 - SOBRE\n");
 	printf("0 - FECHAR PROGRAMA\n");
 	scanf("%c", &opcao);
+
 	return opcao;
 }
 
 char menuClientes(void){
   char opcao;
   system("clear");
+  logoMenuClientes();
+  Usuario* novoUsuario;
+  novoUsuario = (Usuario*) malloc(sizeof(Usuario));
   printf("O que deseja fazer agora?");
   printf("\nEscolha uma opção: ");
   printf("\n1 - Cadastrar Cliente");
   printf("\n2 - Lista de Clientes");
-  printf("\n3 - Modificar Cliente");
-  printf("\n4 - Deletar Cliente");
+  printf("\n3 - Buscar Cliente");
+  printf("\n4 - Modificar Cliente");
+  printf("\n5 - Deletar Cliente");
   printf("\n0 - Voltar ao Menu Principal\n");
   scanf("%c",&opcao);
   while (opcao != '0'){
     switch (opcao){
       case '1':
-      cdCliente();
-      sleep(30);
+      novoUsuario = cdCliente();
+      sleep(10);
       break;
 
       case '2':
-      printf("Em breve.\n");
+      listarClientes(novoUsuario);
       sleep(5);
       break;
 
@@ -65,7 +85,68 @@ char menuClientes(void){
     }
     opcao = menuClientes();
   }
+  free(novoUsuario);
   return opcao;
+}
+
+Usuario* cdCliente(void){
+  system("clear");
+  logoCadastrarCliente();
+  Usuario* usu;
+  usu = (Usuario*) malloc(sizeof(Usuario));
+
+  printf("Digite seu nome: ");
+  scanf(" %80[^\n]", usu->nome);
+
+
+  printf("\nDigite sua data de nascimento: ");
+  scanf("%d/%d/%d", &usu->dia, &usu->mes, &usu->ano);
+  while(!(dataValida(usu->dia, usu->mes, usu->ano))){
+    printf("\nData inválida. Digite novamente: ");
+    scanf("%d/%d/%d", &usu->dia, &usu->mes, &usu->ano);
+  }
+    
+  printf("\nDigite seu email: ");
+  scanf(" %40[^\n]", usu->email);
+  
+  while(!(validaEmail(usu->email))){
+    printf("Email inválido. Digite novamente: ");
+    scanf(" %40[^\n]", usu->email);
+  }
+  
+
+  printf("\nDigite o seu CPF: ");
+	scanf(" %15[^\n]",usu->cpf);
+  
+  while(!(validaCpf(usu->cpf))){
+    printf("CPF inválido. Digite novamente: ");
+    scanf(" %15[^\n]",usu->cpf);
+  }
+    
+  usu->numNativo = numerologia(usu->dia, usu->mes, usu->ano);
+  usu->signos = signo(usu->dia, usu->mes, usu->ano);
+  usu->china = horoscopo(usu->ano);
+
+  usu->status = 'c';
+
+  printf("USUÁRIO CADASTRADO COM SUCESSO!");
+
+
+  printf("\n");
+
+  free(usu);
+  return usu;
+}
+
+void listarClientes(Usuario* usu){
+  printf("Usuários cadastrados: \n");
+  printf("\nNome: %s",usu->nome);
+  printf("\nData de nascimento: %d/%d/%d",usu->dia, usu->mes, usu->ano);
+  printf("\nEmail: %s", usu->email);
+  printf("\nCPF: %s",usu->cpf);
+  printf("\nUsuário nativo do número %d", usu->numNativo);
+  printf("\nSigno: %s", usu->signos);
+  printf("\nAnimal no horóscopo chinês: %s", usu->china);
 }
 
 char menuPrevisoes(void){
@@ -113,4 +194,3 @@ void creditos(void){
 	printf("Email: danrleydaniel21@gmail.com\n");
 	printf("medeiroshiago70@gmail.com\n");
 }
-
