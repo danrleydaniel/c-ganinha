4eaf75bfea137efd867576c68ca1d36e9d9be386
danrleydaniel <danrleydaniel21@gmail.com>
2019-10-18 10:22:39 -0300

    Update menuscgana.h
    Atualização das assinaturas;
    Criação de uma nova estrutura "consultor", que ainda será utilizada no projeto.

--- menuscgana.h
+++ menuscgana.h
@@ -1,4 +1,5 @@
 typedef struct usuario Usuario;
+typedef struct consultor Consultor;
 
 struct usuario{
   char nome[80];
@@ -7,18 +8,30 @@ struct usuario{
   int dia;
   int mes;
   int ano;
-  int numNativo;
-  char* signos; 
-  char* china;
   char status;
+};
+
+struct consultor{
+  char nome[80];
+  char cpf[15];
+  int numNativo;
+  char* signos;
+  char* china;
 };
 
 void logotipoPrincipal(void); //Mostra o logotipo do programa principal
 void logoCadastrarCliente(void); //Mostra o logotipo do menu Cadastrar Cliente
 void logoMenuClientes(void); //Mostra o logotipo do Menu Clientes geral
+void logoListaClientes(void); //Mostra o logotipo do Menu Listar Clientes
+void logoBuscarCliente(void); //Mostra o logotipo do Menu Buscar Cliente
+void logoDeletarCliente(void); //Mostra o logotipo do Menu Deletar Cliente
+void exibeCliente(Usuario*); //Exibe um usuário em específico
 char mainMenu(void); //Menu principal do programa
 char menuClientes(void); //Mostra o menu da área dos clientes
-Usuario* cdCliente(void); //Cadastra um novo usário
-void listarClientes(Usuario*); //Lista os clientes que estão cadastrados (ainda sob implementação)
+void cdCliente(void); //Cadastra um novo usuário
+void gravaUsuario(Usuario*); //Grava os dados de um usuário num arquivo
+void listarClientes(void); //Lista os clientes que estão cadastrados (ainda sob implementação)
+void buscarCliente(void); //Busca um cliente
+void deletarCliente(void);
 char menuPrevisoes(void); //Mostra o menu da área de previsões
 void creditos(void); //Informações sobre os desenvolvedores e o programa
