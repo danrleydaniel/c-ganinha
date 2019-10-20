288f41286b79720c5ea4a93e41696166c460ea9d
danrleydaniel <danrleydaniel21@gmail.com>
2019-10-20 16:09:33 -0300

    Update menuscgana.h
    Atualização das assinaturas.

--- menuscgana.h
+++ menuscgana.h
@@ -15,8 +15,9 @@ struct consultor{
   char nome[80];
   char cpf[15];
   int numNativo;
-  char* signos;
-  char* china;
+  char signos[15];
+  char china[9];
+  char status;
 };
 
 void logotipoPrincipal(void); //Mostra o logotipo do programa principal
@@ -26,12 +27,17 @@ void logoListaClientes(void); //Mostra o logotipo do Menu Listar Clientes
 void logoBuscarCliente(void); //Mostra o logotipo do Menu Buscar Cliente
 void logoDeletarCliente(void); //Mostra o logotipo do Menu Deletar Cliente
 void logoEditarCliente(void); //Mostra o logotipo do Menu Editar Cliente
+void logoMenuPrevisoes(void); //Mostra o logotipo do Menu Previsões
+void logoCadastrarBolaCristal(void); //Mostra o logotipo do Menu Cadastrar na Bola de Cristal
+void logoListarConsultores(void); //Mostra o logotipo do Menu Listar Consultores
+void logoEditarBolaCristal(void); //Mostra o logotipo do Menu Editar Dados da Bola de Cristal
 void exibeCliente(Usuario*); //Exibe um usuário em específico
 char mainMenu(void); //Menu principal do programa
 char menuClientes(void); //Mostra o menu da área dos clientes
 void cdCliente(void); //Cadastra um novo usuário
 void gravaUsuario(Usuario*); //Grava os dados de um usuário num arquivo
-void listarClientes(void); //Lista os clientes que estão cadastrados (ainda sob implementação)
+void gravaConsultor(Consultor*); //Grava os dados de um consultor num arquivo
+void listarClientes(void); //Lista os clientes que estão cadastrados
 void buscarCliente(void); //Busca um cliente
 char menuEditarCliente(void); //Mostra as opções para editar um cliente
 void editarCliente(void); //Edita um cliente
@@ -40,4 +46,8 @@ void editaEmail(Usuario*); //Edita o email
 void editaDataNascimento(Usuario*); //Edita o CPF
 void deletarCliente(void); //Deleta um cliente
 char menuPrevisoes(void); //Mostra o menu da área de previsões
+void cadastraBolaCristal(void); //Cadastra o cliente nas funcionalidades da cigana
+void exibeConsultor(Consultor*); //Exibe um consultor em específico
+void listarConsultores(void); //Lista os consultores que estão cadastrados
+void editarDadosBolaCristal(void); //Edita os dados de algum consultor na bola de cristal
 void creditos(void); //Informações sobre os desenvolvedores e o programa
