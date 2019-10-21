dce02dfafa036210c4da53b24169210c0ef30bb4
danrleydaniel <danrleydaniel21@gmail.com>
2019-10-21 10:18:14 -0300

    Update main.c
    Implementação da função "relatórios".

--- main.c
+++ main.c
@@ -8,29 +8,33 @@
 #include "menuscgana.h"
 
 int main(){
-	char opcao;
-    logotipoPrincipal();
-	opcao = mainMenu();
-	
-	while(opcao != '0'){
-		switch(opcao){
-			case '1':
-			menuClientes();
-			break;
-
-			case '2':
-			menuPrevisoes();
-			break;
-
-			case '3':
-			creditos();
-			break;
-
-			default:
-			printf("\nOpção inválida\n");
+  char opcao;
+  logotipoPrincipal();
+  opcao = mainMenu();
+  
+  while(opcao != '0'){
+    switch(opcao){
+      case '1':
+      menuClientes();
+      break;
+      
+      case '2':
+      menuPrevisoes();
+      break;
+      
+      case '3':
+      relatorios();
+      break;
+      
+      case '4':
+      creditos();
+      break;
+      
+      default:
+      printf("\nOpção inválida\n");
 		}
-	printf("\nO que deseja fazer agora? \n");
-	opcao = mainMenu();
-	}
-	return 0;
+  printf("\nO que deseja fazer agora? \n");
+  opcao = mainMenu();
+  }
+  return 0;
 }
