34ab10c0908d5d3476bf85cf592260bed9400948
danrleydaniel <danrleydaniel21@gmail.com>
2019-11-26 10:49:11 -0300

    Atualização das assinaturas

--- cadastrosevalidacoes.h
+++ cadastrosevalidacoes.h
@@ -7,6 +7,7 @@ char * horoscopo(int); //Função que retorna o animal no horóscopo chinês cor
 char * signo(int, int, int); //Função que retorna o signo de acordo com a data de nascimento do usuário
 int bissexto(int); //Função do professor Flavius Gorgônio que checa se um ano é bissexto
 int dataValida(int, int, int); //Função do professor Flavius Gorgônio que checa se uma data fornecida pelo usário é válida
+int cpfJaExiste(char*); //Verifica se o CPF já está cadastrado
 int validaCpf(char *); //Função que verifica a validez de um CPF dado pelo usário
 int validaEmail(char *); //Função que verifica a validez de um email dado pelo usário
 //Funções que mostram as características de cada signo:
